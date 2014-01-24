1e0352997ecf9d05f323ef81ef35aa6d02aef178
lut23 <luthi003@morris.umn.edu>
2014-01-24 13:43:49 -0600

    Extracted EvenList into its own external, stand-alone class and made all of its fields and methods public.

--- src/applications/EventList.java
+++ src/applications/EventList.java
@@ -1,13 +1,40 @@
 package applications;
 
 public class EventList {
+	// data members
+	int[] finishTime; // finish time array
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	// constructor
+	public EventList(int theNumMachines, int theLargeTime) {// initialize finish times for m machines
+		if (theNumMachines < 1)
+			throw new IllegalArgumentException(MachineShopSimExceptions.NUMBER_OF_MACHINES_MUST_BE_AT_LEAST_1);
+		finishTime = new int[theNumMachines + 1];
 
+		// all machines are idle, initialize with
+		// large finish time
+		for (int i = 1; i <= theNumMachines; i++)
+			finishTime[i] = theLargeTime;
 	}
 
+	/** @return machine for next event */
+	public int nextEventMachine() {
+		// find first machine to finish, this is the
+		// machine with smallest finish time
+		int p = 1;
+		int t = finishTime[1];
+		for (int i = 2; i < finishTime.length; i++)
+			if (finishTime[i] < t) {// i finishes earlier
+				p = i;
+				t = finishTime[i];
+			}
+		return p;
+	}
+
+	public int nextEventTime(int theMachine) {
+		return finishTime[theMachine];
+	}
+
+	public void setFinishTime(int theMachine, int theTime) {
+		finishTime[theMachine] = theTime;
+	}
 }
--- src/applications/MachineShopSimExceptions.java
+++ src/applications/MachineShopSimExceptions.java
@@ -1,7 +1,6 @@
 package applications;
 
 public class MachineShopSimExceptions {
-	
 	public static final String NUMBER_OF_MACHINES_MUST_BE_AT_LEAST_1 = "number of machines must be >= 1";
 	public static final String NUMBER_OF_MACHINES_AND_JOBS_MUST_BE_AT_LEAST_1 = "number of machines and jobs must be >= 1";
 	public static final String CHANGE_OVER_TIME_MUST_BE_AT_LEAST_0 = "change-over time must be >= 0";
