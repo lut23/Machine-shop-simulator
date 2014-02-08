119277d543fb238e040fed5023bae0bbe5e0897c
lut23 <luthi003@morris.umn.edu>
2014-02-08 16:13:27 -0600

    Made get/set method for finishTime in EventList.java.  Removed set method for finishTime in Event.java.

--- src/applications/EventList.java
+++ src/applications/EventList.java
@@ -2,7 +2,7 @@ package applications;
 
 public class EventList {
 	// data members
-	int[] finishTime; // finish time array
+	private int[] finishTime; // finish time array
 
 	// constructor
 	public EventList(int theNumMachines, int theLargeTime) {// initialize finish times for m machines
@@ -13,7 +13,7 @@ public class EventList {
 		// all machines are idle, initialize with
 		// large finish time
 		for (int i = 0; i < theNumMachines; i++)
-			finishTime[i+1] = theLargeTime;
+			getFinishTime()[i+1] = theLargeTime;
 	}
 
 	/** @return machine for next event */
@@ -21,20 +21,24 @@ public class EventList {
 		// find first machine to finish, this is the
 		// machine with smallest finish time
 		int fastestMachineIndex = 1;
-		int smallestFinishTime = finishTime[1];
-		for (int i = 1; i < finishTime.length-1; i++)
-			if (finishTime[i+1] < smallestFinishTime) {// i finishes earlier
+		int smallestFinishTime = getFinishTime()[1];
+		for (int i = 1; i < getFinishTime().length-1; i++)
+			if (getFinishTime()[i+1] < smallestFinishTime) {// i finishes earlier
 				fastestMachineIndex = i+1;
-				smallestFinishTime = finishTime[i+1];
+				smallestFinishTime = getFinishTime()[i+1];
 			}
 		return fastestMachineIndex;
 	}
 
 	public int nextEventTime(int theMachine) {
-		return finishTime[theMachine];
+		return getFinishTime()[theMachine];
 	}
 
 	public void setFinishTime(int theMachine, int theTime) {
 		finishTime[theMachine] = theTime;
+	}
+
+	int[] getFinishTime() {
+		return finishTime;
 	}
 }
